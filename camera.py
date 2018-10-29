c2ce0316210500d7eeb02537bfcfdb292fc33f4b
VesaPetays <vesa.petays@student.tut.fi>
2018-10-29 13:55:04 +0200

    Kamera sarjatulella

--- camera.py
+++ camera.py
@@ -4,14 +4,26 @@ from time import sleep
 #print("Hello there!")
 
 camera = PiCamera()
-camera.resolution = (640, 480)
+#camera.resolution = (640, 480)
+camera.resolution = (320, 200)
 
 camera.rotation = 270
 
 #camera.start_preview(alpha=200)
 camera.start_preview()
+
 # It’s important to sleep for at least 2 seconds before capturing,
 # to give the sensor time to set its light levels.
+
+for i in range(2):
+    sleep(3)
+    camera.capture('/home/pi/Desktop/image%s.jpg' % i)
+camera.stop_preview()
+
+
+
+camera.start_preview()
+camera.start_recording('/home/pi/video.h264')
 sleep(5)
-camera.capture('/home/pi/Desktop/image.jpg')
+camera.stop_recording()
 camera.stop_preview()
