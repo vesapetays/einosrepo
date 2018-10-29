143348586f13757340c57e8b11b350fcb5a09f6d
VesaPetays <vesa.petays@student.tut.fi>
2018-10-29 14:12:37 +0200

    kehitys kehittyy

--- camera.py
+++ camera.py
@@ -6,11 +6,15 @@ from time import sleep
 camera = PiCamera()
 #camera.resolution = (640, 480)
 camera.resolution = (320, 200)
+#camera.resolution = (64, 64)
 
 camera.rotation = 270
 
 #camera.start_preview(alpha=200)
 camera.start_preview()
+camera.annotate_text = "Heippa, olen Vesa"
+
+camera.brightness = 55
 
 # It’s important to sleep for at least 2 seconds before capturing,
 # to give the sensor time to set its light levels.
@@ -21,8 +25,11 @@ for i in range(2):
 camera.stop_preview()
 
 
+#video
+camera.framerate = 30
 
 camera.start_preview()
+camera.annotate_text = "Hello world!"
 camera.start_recording('/home/pi/video.h264')
 sleep(5)
 camera.stop_recording()
